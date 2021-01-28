702e736d41428ff4ea4a892ecf63d53f23826a4d
Z. Wen <45642009+zwentt@users.noreply.github.com>
2021-01-28 09:58:01 -0500

    Update BayesianEstimationModels.R

--- Data_Analysis/BayesianEstimationModels.R
+++ Data_Analysis/BayesianEstimationModels.R
@@ -24,14 +24,19 @@ gigrcolv.cross.brm <- brm(outcome ~ agility2cj + gigrcolv_c + firmsizestd + netw
 
 ggndv.cross.brm <- brm(outcome ~ agility2cj + ggndv_c + firmsizestd + network2xcj + gmci2016c + I(firmsizestd*firmsizestd) + ggndv_c*agility2cj +  (ggndv_c | countryx), data = CultureData)
 
+agility.ginscolv.cross.brm <- brm(sensing2 ~ ginscolv_c + firmsizestd + network2xcj + gmci2016c + ginscolv_c*network2xcj +  (ginscolv_c | countryx), data = CultureData)
+
+
+
+# Assertiveness using domestic firms only
 gassv.cross.brm <- brm(outcome ~ agility2cj + gassv_c + firmsizestd + network2xcj + gmci2016c + I(firmsizestd*firmsizestd) + gassv_c*agility2cj +  (gassv_c | countryx), data = subset(CultureData, network2x == 0))
 
 
+# Song et al., 2018 IJPE
+# "To specify prior distributions for the parameters, we used diffuse priors similar to the ones used in Stegmueller (2013). To estimate the posterior distributiosn of interests, we adopted MCMC methods. 
 
-
-
-agility.ginscolv.cross.brm <- brm(sensing2 ~ ginscolv_c + firmsizestd + network2xcj + gmci2016c + ginscolv_c*network2xcj +  (ginscolv_c | countryx), data = CultureData)
-
+# Stegmuller, 2013, American Journal of Political Science 
+# "Results presented used inverse gamma priors with small values for shape and scale, which showed to yeild reasonable results in my simulations, despite their technical short-comings (Gelman, 2006). The difference between prior choices is illustrated in Figure 7, which shows interval coverage for country-level covariate effects gamma_1 in a hierarchical linear model under different prior specifications. The slight difference between inverse-gamma and uniform on the standard deviation priors vanish as more information becomes available. This pattern is similar for other coefficient estimates in both linear and probit models. When inverse Wishart priors are used as priors for variance-covariance matrices, I find similar results: differences in prior values are only relevant when little country-level information is available. This suggests that applied researchers who have to work with small samples should test the robustness 
 
 
 
